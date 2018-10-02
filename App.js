919ce578747167991f3c665ce941aa44241c40e1
haneulee <lovesky4294@gmail.com>
2018-10-02 15:35:02 +0900

    build

--- App.js
+++ App.js
@@ -46,6 +46,7 @@ export default class App extends React.Component {
             returnKeyType={"done"}
             autoCorrect={false}
             onSubmitEditing={this._addToDo}
+            underlineColorAndroid={"transparent"}
           />
           <ScrollView contentContainerStyle={styles.toDos}>
             {/* <ToDo text={"hello, "} /> */}
